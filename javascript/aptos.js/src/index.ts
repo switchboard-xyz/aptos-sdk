b2da234eff6efe172ab98edec9256e9f9f6a1f3c
Conner Gallagher <ConnerNGallagher@gmail.com>
2022-12-16 12:32:08 -0700

    added SwitchboardProgram wrapper

--- javascript/aptos.js/src/index.ts
+++ javascript/aptos.js/src/index.ts
@@ -1,11 +1,13 @@
-export * as types from "./generated/index.js";
-export * as errors from "./generated/errors.js";
+export * as types from "./generated";
+export * as errors from "./generated";
 
 export {
   TESTNET_PROGRAM_ID,
   MAINNET_PROGRAM_ID,
   DEVNET_PROGRAM_ID,
   PROGRAM_ID,
-} from "./generated/index.js";
+} from "./generated";
 
-export * from "./sbv2.js";
+export * from "./SwitchboardProgram";
+
+export * from "./sbv2";
